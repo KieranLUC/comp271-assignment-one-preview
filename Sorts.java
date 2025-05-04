409848949582d60256d6e9199b43edb327b915a3
dylanbass88 <dbass@luc.edu>
2025-05-04 21:26:15 +0400

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -5,11 +5,23 @@ public class Sorts {
         System.out.println("Original:");
         printArray(numbers);
 
-        // Example: Use Selection Sort
-        selectionSort(numbers);
+        // Run Selection Sort
+        int[] selectionSorted = numbers.clone();
+        selectionSort(selectionSorted);
+        System.out.println("Selection Sort:");
+        printArray(selectionSorted);
 
-        System.out.println("Sorted:");
-        printArray(numbers);
+        // Run Merge Sort
+        int[] mergeSorted = numbers.clone();
+        mergeSort(mergeSorted, 0, mergeSorted.length - 1);
+        System.out.println("Merge Sort:");
+        printArray(mergeSorted);
+
+        // Run Quick Sort
+        int[] quickSorted = numbers.clone();
+        quickSort(quickSorted, 0, quickSorted.length - 1);
+        System.out.println("Quick Sort:");
+        printArray(quickSorted);
     }
 
     public static void printArray(int[] arr) {
@@ -18,90 +30,73 @@ public class Sorts {
         }
         System.out.println();
     }
-public static void selectionSort(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) arr[k++] = L[i++];
+            else arr[k++] = R[j++];
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    // Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
             }
         }
-        int temp = arr[minIndex];
-        arr[minIndex] = arr[i];
-        arr[i] = temp;
+
+        int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
+        return i + 1;
     }
-}
-public static void mergeSort(int[] arr, int left, int right) {
-    if (left < right) {
-        int mid = (left + right) / 2;
-
-        // Sort left half
-        mergeSort(arr, left, mid);
-
-        // Sort right half
-        mergeSort(arr, mid + 1, right);
-
-        // Merge both halves
-        merge(arr, left, mid, right);
-    }
-}
-
-public static void merge(int[] arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    int[] L = new int[n1];
-    int[] R = new int[n2];
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k++] = L[i++];
-        } else {
-            arr[k++] = R[j++];
-        }
-    }
-
-    while (i < n1) {
-        arr[k++] = L[i++];
-    }
-
-    while (j < n2) {
-        arr[k++] = R[j++];
-    }
-}
-public static void quickSort(int[] arr, int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        // Recursively sort elements before and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-public static int partition(int[] arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-
-    return i + 1;
 }
